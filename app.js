1e96e70be2ae98346997483ebdaa1c64fcb63a40
whitee00 <138634064+whitee00@users.noreply.github.com>
2024-11-06 22:13:56 -0300

    2

--- app.js
+++ app.js
@@ -1,22 +1,35 @@
-let flashcard = documet.getElementById('flashcard');
-    let cartao = document.createElement('article');
+function criaCartao (item, pergunta, resposta) {
+    //console.log(item, pergunta,resposta)
+
+    let flascard = document.getElementById('flashcard')
+    let cartao = document.createElement('article')
 
     cartao.className = 'cartao';
 
     cartao.innerHTML = `
-        <div class="cartao__conteudo">
-            <h3>Missão</h3>
-            
-            <div class="cartao__conteudo__pergunta">
-                <p>Saiba qual é a missão de nossa empresa!</p>
+      <div class="cartao__conteudo">
+                <h3>Missão</h3>
+                
+                <div class="cartao__conteudo__pergunta">
+                    <p>Saiba qual é a missão de nossa empresa!</p>
+                </div>
+                
+                <div class="cartao__conteudo__resposta">
+                    <p>Nossa missão é ... </p>
+                </div>
             </div>
+            `
+
+      
+            let respostaEstaVisivel = false;
             
-            <div class="cartao__conteudo__resposta">
-                <p>Nossa missão é ... </p>
-            </div>
-        </div>
-    `
+            function viraCartao () {
+              respostaEstaVisivel = !variavelEstaVisivel;
+              cartao.classList.toogle('active', viraCartao);
+    }
+
+    cartao.addEventListener('click', viraCartao);
+
     flashcard.appendChield(cartao);
 
-}function criaCartao (item, pergunta, resposta) {
-    //console.log(item, pergunta,resposta)
+}
