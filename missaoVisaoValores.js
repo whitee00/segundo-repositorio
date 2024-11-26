b6b48ca36bc5fdb3936abdd204ba9274602b2b3a
whitee00 <138634064+whitee00@users.noreply.github.com>
2024-11-26 20:14:05 -0300

    wJEUf1

--- missaoVisaoValores.js
+++ missaoVisaoValores.js
@@ -1,21 +1,16 @@
 criaCartao (
     'Missão',
     'Saiba qual é a missão de nossa empresa!',
-    * Forros de gesso e drywall de alta qualidade
-    * Dar satisfação total ao cliente 
-    * Ser reconhecido mundialmente.
+    'Forros de gesso e drywall de alta qualidade Dar satisfação total ao cliente  Ser reconhecido mundialmente.'
 )
 
 criaCartao (
     'Visão',
-    'Saiba qual é a visão de nossa empresa!',
-    * Ser reconhecido por fazer as melhores 
-      decorações em casa de celebridades. 
+    'Saiba qual é a visão de nossa empresa!'
+    'Ser reconhecido por fazer as melhores decorações em casa de celebridades.' 
 )
 
 criaCartao (
     'Valores',
     'Saiba quais são os valores de nossa empresa!',
-    * Tratar os clientes e os funcionarios bem,
-      sempre pedindo fedebeks para sempre 
-      evoluir. 
+    'Tratar os clientes e os funcionarios bem,sempre pedindo fedebeks para sempre evoluir.'
