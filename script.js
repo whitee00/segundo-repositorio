c899e7d70adfa1dbbc0942c92e1c4bcfd21106cb
whitee00 <138634064+whitee00@users.noreply.github.com>
2025-04-07 22:51:59 -0300

    07/04

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const aumentaFonteBotao = document.getElementById
+    ('aumentarfonte');
+    aumentaFonteBotao.addEventListener('click', function(){ 
+
+     })
+     Const botaoDeAcessibilidade = document.getElementById
+     ('botao-acessibilidade');
+     const opcoesDeAcessibilidade = document.getElementById
+     ('opcoes-acessibilidade');
+
+    })
